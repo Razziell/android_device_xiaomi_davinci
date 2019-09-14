9423e42f8dd406d4221d65ff32376df0a60f78bc
LuK1337 <priv.luk@gmail.com>
2019-09-15 00:45:36 +0200

    bluetooth: Push×pop PROPERTY_VALUE_MAX in buildcfg header
    This deals with the PROPERTY_VALUE_MAX redefinition compilation issue.
    
    Change-Id: I34bb283512ca1a75c680af140846f16cccf0c645

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -23,6 +23,8 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
+#pragma push_macro("PROPERTY_VALUE_MAX")
+
 #include <cutils/properties.h>
 #include <string.h>
 
@@ -37,4 +39,7 @@
 #define BT_CLEAN_TURN_ON_DISABLED TRUE
 // Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
 #define AVDT_NUM_SEPS 12
+
+#pragma pop_macro("PROPERTY_VALUE_MAX")
+
 #endif
