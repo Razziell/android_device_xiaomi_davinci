68f07f3aa7344848d217cf209d4a25f8ab473208
Razziell <razziell@ya.ru>
2019-11-07 15:11:38 +0300

    interfaces: fix build

--- interfaces/update-makefiles.sh
+++ interfaces/update-makefiles.sh
@@ -3,4 +3,4 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.xiaomi:device/xiaomi/sm6150-common/interfaces"
+  "vendor.xiaomi:device/xiaomi/davinci/interfaces"
