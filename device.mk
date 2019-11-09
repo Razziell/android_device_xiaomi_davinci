0c5f9676dd2d88ae7d8a46cebf4bc5554e58dfc3
Razziell <razziell@ya.ru>
2019-11-09 23:27:48 +0300

    Disable wfd (temp) & more

--- device.mk
+++ device.mk
@@ -462,8 +462,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libqdMetaData.system
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
