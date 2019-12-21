13f2ec552b781a245c591b30ebe63016f2364365
Razziell <razziell@ya.ru>
2019-12-21 22:50:58 +0300

    davinci: Switch to qsee cryptfshw implementation

--- device.mk
+++ device.mk
@@ -264,7 +264,7 @@ PRODUCT_COPY_FILES += \
 
 # HW crypto
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-ioctl-qti
+    vendor.qti.hardware.cryptfshw@1.0-impl-qti.qsee
 
 # Init scripts
 PRODUCT_PACKAGES += \
