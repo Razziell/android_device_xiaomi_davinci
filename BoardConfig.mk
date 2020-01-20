7482a72e16bc419ec01bccbe6092034b3d13b67f
Razziell <razziell@ya.ru>
2020-01-20 19:10:54 +0300

    Boardconfig: use pancake kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,8 +103,8 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_SOURCE := kernel/xiaomi/davinci
-TARGET_KERNEL_CONFIG := vendor/lineage_davinci_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/pancake
+TARGET_KERNEL_CONFIG := vendor/davinci_defconfig
 TARGET_KERNEL_VERSION := 4.14
 
 # Platform
