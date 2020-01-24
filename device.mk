680b92a68da29f773de08f622f9560c94691f4eb
Razziell <razziell@ya.ru>
2020-01-24 10:30:52 +0300

    davinci: use prebuild camera stack

--- device.mk
+++ device.mk
@@ -166,12 +166,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0 \
-    camera.device@3.2-impl \
-    camera.device@1.0-impl \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
     libgui_vendor \
     libdng_sdk.vendor \
     libxml2 \
