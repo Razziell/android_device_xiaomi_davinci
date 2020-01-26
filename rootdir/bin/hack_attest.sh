78dbae0c259c88106f767cf82996d58b04c54327
Razziell <razziell@ya.ru>
2020-01-26 16:58:37 +0300

    rootdir: more update gpay hack

--- rootdir/bin/hack_attest.sh
+++ rootdir/bin/hack_attest.sh
@@ -2,21 +2,20 @@
 
 sleep 2
 # Attest method hack google pay
-# stop Google Pay
-/system/bin/am force-stop /data/data/com.google.android.apps.walletnfcrel
-sleep 2
+if [ -f /data/data/com.google.android.gms/databases/dg.db ]; then
+    # stop Google Pay
+    /system/bin/am force-stop /data/data/com.google.android.apps.walletnfcrel
+    sleep 2
 
-if [ -w /data/data/com.google.android.gms/databases/dg.db ]; then
     rm -rf /data/data/com.google.android.gms/app_dg_cache/*
+    sleep 2
 
     # set 777 permissions on dg.db
-    chmod 777 /data/data/com.google.android.gms/databases/dg.db
+    /system/bin/chmod 777 /data/data/com.google.android.gms/databases/dg.db
     sleep 2
 
-    sqlite3 -batch /data/data/com.google.android.gms/databases/dg.db "update main set c=0 where a like '%attest%'"
+    /system/bin/sqlite3 /data/data/com.google.android.gms/databases/dg.db "update main set c='0' where a like '%attest%';"
     sleep 2
-    
-    if [ /vendor/bin/toybox_vendor stat -c "%a" $database != "440" ]; then
-        chmod 440 /data/data/com.google.android.gms/databases/dg.db
-    fi
+
+    /system/bin/chmod 440 /data/data/com.google.android.gms/databases/dg.db
 fi
