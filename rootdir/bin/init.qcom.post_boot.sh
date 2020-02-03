991543ab58199aac2a4b128256d0ced8dc66d645
Razziell <razziell@ya.ru>
2020-02-03 19:37:40 +0300

    rootdir: post_boot: remove gpu min level

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -348,7 +348,7 @@ case "$target" in
             echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
 
             # add GPU frequency adjust 
-            echo 267 > /sys/devices/platform/soc/5000000.qcom,kgsl-3d0/kgsl/kgsl-3d0/min_clock_mhz
+            #echo 267 > /sys/devices/platform/soc/5000000.qcom,kgsl-3d0/kgsl/kgsl-3d0/min_clock_mhz
 
             # Set Memory parameters
             configure_memory_parameters
