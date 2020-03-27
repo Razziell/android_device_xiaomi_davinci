Mani30R/device_xiaomi_davinci

davinci: fod clean hal and bump to version 1.1
 * Clean unused funcions
 * Use fwk provided brightness

Change-Id: Iab7909b797eb1c049defc403c71bbd4cfecfe15d

fod/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "lineage.biometrics.fingerprint.inscreen@1.0-service.davinci"
+#define LOG_TAG "lineage.biometrics.fingerprint.inscreen@1.1-service.davinci"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
@@ -24,8 +24,8 @@
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-using vendor::lineage::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreen;
-using vendor::lineage::biometrics::fingerprint::inscreen::V1_0::implementation::FingerprintInscreen;
+using vendor::lineage::biometrics::fingerprint::inscreen::V1_1::IFingerprintInscreen;
+using vendor::lineage::biometrics::fingerprint::inscreen::V1_1::implementation::FingerprintInscreen;
 
 using android::OK;
 using android::status_t;

Razziell/android_device_xiaomi_davinci

fod/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_davinci"
+#define LOG_TAG "lineage.biometrics.fingerprint.inscreen@1.1-service.xiaomi_davinci"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
@@ -24,8 +24,8 @@
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-using vendor::lineage::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreen;
-using vendor::lineage::biometrics::fingerprint::inscreen::V1_0::implementation::FingerprintInscreen;
+using vendor::lineage::biometrics::fingerprint::inscreen::V1_1::IFingerprintInscreen;
+using vendor::lineage::biometrics::fingerprint::inscreen::V1_1::implementation::FingerprintInscreen;
 
 using android::OK;
 using android::status_t;