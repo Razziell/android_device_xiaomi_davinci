53f93d581801594ef239e2910beee658a91f4c0e
Razziell <razziell@ya.ru>
2021-06-22 20:21:07 +0300

    davinci: update stock security patch (01.05.2021)

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -60,12 +60,11 @@ void load_davinciglobal() {
     property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi 9T");
     property_override("ro.build.product", "davinci");
     property_override_dual("ro.product.device", "ro.product.vendor.device", "davinci");
-    property_override("ro.build.description", "davinci-user 10 QKQ1.190825.002 V12.0.8.0.QFJMIXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.8.0.QFJMIXM:user/release-keys");
+    property_override("ro.build.description", "davinci-user 10 QKQ1.190825.002 V12.0.9.0.QFJMIXM release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.9.0.QFJMIXM:user/release-keys");
     property_override("ro.com.google.clientidbase", "android-xiaomi");
     property_override("ro.com.google.clientidbase.ms", "android-xiaomi-rev1");
     property_override("ro.product.mod_device", "davinci_global");
-    property_override("ro.build.type", "user");
     property_override("ro.boot.verifiedbootstate", "green");
 }
 
@@ -73,12 +72,11 @@ void load_davinci() {
     property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi 9T");
     property_override("ro.build.product", "davinci");
     property_override_dual("ro.product.device", "ro.product.vendor.device", "davinci");
-    property_override("ro.build.description", "davinci-user 10 QKQ1.190825.002 V12.0.8.0.QFJMIXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.8.0.QFJMIXM:user/release-keys");
+    property_override("ro.build.description", "davinci-user 10 QKQ1.190825.002 V12.0.9.0.QFJMIXM release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.9.0.QFJMIXM:user/release-keys");
     property_override("ro.com.google.clientidbase", "android-xiaomi");
     property_override("ro.com.google.clientidbase.ms", "android-xiaomi-rev1");
     property_override("ro.product.mod_device", "davinci_global");
-    property_override("ro.build.type", "user");
     property_override("ro.boot.verifiedbootstate", "green");
 }
 
