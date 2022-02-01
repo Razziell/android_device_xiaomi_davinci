059d29ec55ed19fa52ee16134d67de8a7fb1bfcd
Razziell <razziell@ya.ru>
2022-02-01 21:23:10 +0300

    Boardconfig: fix typo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,7 +166,7 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.propop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
