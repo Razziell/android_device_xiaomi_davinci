04a3ef1e23616d1a178cdefbf717646fc5598911
Razziell <razziell@ya.ru>
2022-02-08 21:48:16 +0300

    davinci: Build mock hidl power stats

--- device.mk
+++ device.mk
@@ -328,7 +328,8 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
