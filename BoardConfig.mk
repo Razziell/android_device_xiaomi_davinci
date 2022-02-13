d14abc0041f0676edfc5adeee5ad784324a98815
Razziell <razziell@ya.ru>
2022-02-13 10:16:01 +0300

    Boardconfig: disable per-cgroup pressure tracking

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,6 +112,7 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += cgroup_disable=pressure
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
