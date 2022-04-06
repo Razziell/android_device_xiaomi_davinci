0abc8538d95da0de0c50a3e4ab837253cdae978a
Razziell <razziell@ya.ru>
2022-04-06 20:59:50 +0300

    init: rework property_override_dual

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -90,6 +90,7 @@ void vendor_load_properties() {
     std::string fingerprint;
     std::string description;
     std::string mod_device;
+    std::string build_type;
 
     if (region == "GLOBAL") {
         model = "Mi 9T";
@@ -118,13 +119,9 @@ void vendor_load_properties() {
         property_override("ro.product.mod_device", mod_device.c_str());
     }
 
-    std::string build_type;
-    build_type = GetProperty("ro.build.type", "");
-    if (build_type == "userdebug") {
-        property_override_dual("ro.build.type", "ro.vendor.build.type", "user");
-        property_override_dual("ro.odm.build.type", "ro.product.build.type", "user");
-        property_override_dual("ro.system.build.type", "ro.system_ext.build.type", "user");
-    }
+    property_override_dual("ro.build.type", "ro.vendor.build.type", "user");
+    property_override_dual("ro.odm.build.type", "ro.product.build.type", "user");
+    property_override_dual("ro.system.build.type", "ro.system_ext.build.type", "user");
 
     property_override("ro.boot.hardware.revision", hardware_revision.c_str());
     property_override("ro.boot.verifiedbootstate", "green");
